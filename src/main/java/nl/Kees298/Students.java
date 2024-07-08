b2ce9d6e29984d67f914af2a5ca72b73f8c5ba06
Kees <k.manshanden@gmail.com>
2024-07-08 14:30:49 +0200

    switch 2 works too Tabs are still a bit ugly.

--- src/main/java/nl/Kees298/Students.java
+++ src/main/java/nl/Kees298/Students.java
@@ -13,6 +13,7 @@ class Students {
 		String nameStudent = "";
 		int ageStudent = 0;
 		while (true) {
+			System.out.println();
 			System.out.println("Dit is een programma die de naam  en leeftijd van studenten opslaat.");
 			System.out.println("Type '1' om een nieuwe student toe te voegen.");
 			System.out.println("Type '2' om een student te verwijderen.");
@@ -23,7 +24,7 @@ class Students {
 			switch (choice) {
 				case 1:
 					System.out.print("Hoe heet de student die je wilt toevoegen? ");
-					while (Objects.equals(nameStudent, "")) { //
+					while (Objects.equals(nameStudent, "")) { //only run this when no name has been assigned yet.
 						nameStudent = input.nextLine();
 					}
 					System.out.println("Naam: " + nameStudent);
@@ -39,15 +40,21 @@ class Students {
 
 				case 2:
 					System.out.print("Hoe heet de student die je wilt verwijderen? ");
+					while (Objects.equals(nameStudent, ""))
 					nameStudent = input.nextLine();
 					if (person.containsKey(nameStudent)) { //to prevent error if there's a typo.
+						System.out.println(nameStudent + " is verwijderd uit de lijst.");
 						person.remove(nameStudent);
+
 					} else
 						System.out.println(nameStudent + " is niet gevonden in het bestand");
+					nameStudent = ""; // reset for further students
+					ageStudent = 0; //reset for further students
 					break;
 				case 3:
+					System.out.println("Namen \t\t\t Leeftijden"); // heading for the list.
 					for (String i : person.keySet()) {
-						System.out.println("Naam: " + i + "\t\t\t Leeftijd: " + person.get(i));
+						System.out.println( i + "\t\t\t" + person.get(i));
 					}
 					break;
 			}
